d4bc1c7b767df1cfa77550335c5e83873e9a2e96
Manuel <82036608+jsfiddler@users.noreply.github.com>
2021-04-16 16:35:06 +0200

    Update forwardSearch2.js

--- forwardSearch2.js
+++ forwardSearch2.js
@@ -9,11 +9,14 @@ await fetch(url)
   .then((response)=>{return response.text()})
   .then((data)=>{
 	  console.clear();
-	  /*console.log(data); */
+	  /*create an XML-parser */
 	  var parser = new DOMParser ();
-	  pnNumbers = parser.parseFromString (data, 'text/xml').children[0].children[0].children[2].children;
+	  pnNumbers = parser.parseFromString (data, 'text/xml');
+	  /*Go to the respective child of the XML-DOM element*/
+	  pnNumbers=pnNumbers.children[0].children[0].children[2].children;
+	  /*Loop to get the textContent of this and do some text-operation on it*/
 	  for (i=0;i< pnNumbers.length;i++) {txt+=pnNumbers[i].textContent.replaceAll(' ','').replaceAll('\n','')+','};
-	  console.log(txt);
-	  window.open('https://worldwide.espacenet.com/patent/search?q=ct any "'+encodeURIComponent(txt.slice(0, -1))+'"');
+	  /*Open a new window and paste this string. */
+	  window.open('https://worldwide.espacenet.com/patent/search?q=ct any "'+encodeURIComponent(txt.slice(0, -1))+'"','forwardSearch');
 	  })
 .catch(err=>console.log(err));
