60cf867bcafaf6891f8b4babfa64dff6d11e022f
Manuel <82036608+jsfiddler@users.noreply.github.com>
2021-05-07 18:53:45 +0200

    Update hideNavbar.js

--- readability/hideNavbar.js
+++ readability/hideNavbar.js
@@ -1,10 +1,14 @@
 javascript:
-if (!Number(sessionStorage.getItem('readability'))==1) { 
+/*is "readability in sessionStorage 0, undefined or null?*/
+if (!sessionStorage.getItem('readability')==1) { 
+	/*Create a new key-value pair in sessionStorage.*/
 	sessionStorage.setItem('readability',1);
+	/*hide the navbar*/
 	document.querySelector('nav').setAttribute('style','display:none');
 	document.querySelector('[class*=breadcrumbs]').setAttribute('style','display:none');
 }
 else {
+	/*seems like you have hidden it- undo those changes!*/
 	sessionStorage.setItem('readability',0); 
 	document.querySelector('nav').setAttribute('style','display:visible');
 	document.querySelector('[class*=breadcrumbs]').setAttribute('style','display:visible');
