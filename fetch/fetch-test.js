58c6790827deb98cdebefa0390eace44339c6bb9
Manuel <82036608+jsfiddler@users.noreply.github.com>
2023-07-19 19:49:10 +0200

    Delete fetch directory

--- fetch/fetch-test.js
+++
@@ -1,9 +0,0 @@
-var URL=`https://worldwide.espacenet.com/3.2/rest-services/published-data/publication/docdb/EP1000000/biblio.json`
-await fetch(URL)
-.then((response) => {return response.json()})
-.then( (data) => { 
-	console.group("data returned from FETCH request:");
-	console.dir(data);    
-	console.groupEnd;    
-})
-.catch(err=>console.log(err))  
